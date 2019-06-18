7cd7e3c853c558bff6efeeae140f4d3618a37cbf
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-06-18 19:39:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,16 +1,16 @@
-def reverse_each_word(str)
-  arr = str.split(" ")
-  result = []
-  arr.each do |x|
-    result << x.reverse
-  end
-  return result.join(" ")
-end
-
 # def reverse_each_word(str)
 #   arr = str.split(" ")
-#   arr.collect do |x|
-#     x.reverse
+#   result = []
+#   arr.each do |x|
+#     result << x.reverse
 #   end
-#   return arr.join(" ")
+#   return result.join(" ")
 # end
+
+def reverse_each_word(str)
+  arr = str.split(" ")
+  arr.collect do |x|
+    x.reverse
+  end
+  return arr.join(" ")
+end
